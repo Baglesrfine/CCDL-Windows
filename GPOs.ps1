f7fe73edc9e50ad470d437c50dcea1beb9c38bc3
Rexantor96 <willworthin@outlook.com>
2025-03-06 16:38:28 -0800

    Final updates

--- GPOs.ps1
+++ GPOs.ps1
@@ -1,6 +1,5 @@
 $ccdcPath = "C:\CCDC"
 $toolsPath = "$ccdcPath\tools-Windows"
-mkdir "$toolsPath\GPOs"
 
 # Download Group Policy
 $tools = @(
@@ -30,38 +29,44 @@ foreach ($tool in $tools) {
 #$wmiFilterFolder = "$env:TEMP\DoD_GPOs\DoD WinSvr 2019 MS and DC v3r2\WMI Filter"
 $gpoFolders = @()
 
-# Select the GPOs based on the OS
+# Select the GPOs based on the OS and server role
 $productName = (Get-ComputerInfo).WindowsProductName
 if ($productName -eq "Windows Server 2019 Standard") {
     if ((Get-WindowsFeature -Name AD-Domain-Services).installed) {
         $gpoFolders = @(
-            @{ Name = 'Defender'; Path = "$toolsPath\Windows Defender GPOs"},
-            @{ Name = 'Firefox'; Path = "$toolsPath\Firefox GPOs"},
-            @{ Name = 'Edge'; Path = "$toolsPath\Edge GPOs"},
-            @{ Name = 'Windows 2019'; Path = "$toolsPath\Windows 2019 GPOs"}
+            # @{ Name = 'Defender'; Path = "$toolsPath\Windows Defender GPOs"},
+            # @{ Name = 'Firefox'; Path = "$toolsPath\Firefox GPOs"},
+            # @{ Name = 'Edge'; Path = "$toolsPath\Edge GPOs"},
+            @{ Name = 'Windows 2019 DC'; Path = "$toolsPath\Windows 2019 GPOs"; Id = "515BB8D8-8316-445B-8EDF-590B1B434EC8"}
+        )
+    } else {
+        $gpoFolders = @(
+            # @{ Name = 'Defender'; Path = "$toolsPath\Windows Defender GPOs"},
+            # @{ Name = 'Firefox'; Path = "$toolsPath\Firefox GPOs"},
+            # @{ Name = 'Edge'; Path = "$toolsPath\Edge GPOs"},
+            @{ Name = 'Windows 2019 MS'; Path = "$toolsPath\Windows 2019 GPOs"; Id = "314F919C-F030-469C-89C4-6B65CE42AC9D"}
         )
     }
 }
 else {
     $gpoFolders = @(
-        @{ Name = 'Defender'; Path = "$toolsPath\Windows Defender GPOs"},
-        @{ Name = 'Firefox'; Path = "$toolsPath\Firefox GPOs"},
-        @{ Name = 'Edge'; Path = "$toolsPath\Edge GPOs"},
-        @{ Name = 'Windows 2019'; Path = "$toolsPath\Windows 2019 GPOs"}
+        # @{ Name = 'Defender'; Path = "$toolsPath\Windows Defender GPOs"},
+        # @{ Name = 'Firefox'; Path = "$toolsPath\Firefox GPOs"},
+        # @{ Name = 'Edge'; Path = "$toolsPath\Edge GPOs"},
+        @{ Name = 'Windows 10'; Path = "$toolsPath\Windows 10 GPOs"; Id = "11CEE829-941A-4704-B7DC-2880B3D3710E"}
     )
 }
 
 # Import the GPOs
 Write-Host "Importing DoD GPOs..."
 foreach ($gpoFolder in $gpoFolders) {
-    $gpoSubFolders = Get-ChildItem -Path $gpoFolder.Path -Directory
-    foreach ($gpoSubFolder in $gpoSubFolders) {
-        $gpoName = $gpoSubFolder.Name
-        Write-Host "Importing GPO: $gpoName"
-        New-GPO -Name $gpoFolder.Name
-        $gpoId = (Get-GPO -Name $gpoFolder.Name).Id
-        Import-GPO -BackupId $gpoId -Path $gpoFolder.Path -TargetGuid $gpoId
-    }
+    $gpoName = $gpoFolder.Name
+    Write-Host "Importing GPO: $gpoName"
+    New-GPO -Name $gpoFolder.Name
+    $gpoId = (Get-GPO -Name $gpoFolder.Name).Id
+    Import-GPO -BackupId $gpoFolder.Id -Path $gpoFolder.Path -TargetGuid $gpoId
+    # Link the GPO to the domain and enforce it
+    Write-Host "GPO has been created, enforce on the next boot."
 }
 
 # Apply WMI filters if they exist
